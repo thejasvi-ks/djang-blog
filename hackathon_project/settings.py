0e415bd689b6b9ec38b1bf47aeb87542b3ebdfb7
Thejasvi K S <tks@stats.com>
2019-10-21 20:38:43 +0530

    Updated Settings

--- hackathon_project/settings.py
+++ hackathon_project/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&g&(r13jn&2a#*wy(=wd06=g9z^xdoof-4-$3)f%x^%+x7du=n'
+# SECRET_KEY = '&g&(r13jn&2a#*wy(=wd06=g9z^xdoof-4-$3)f%x^%+x7du=n'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['theja-django-app.herokuapp.com']
 
 
 # Application definition
